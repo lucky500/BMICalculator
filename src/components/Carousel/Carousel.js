b8269949b58d3e4746e68af0bc62c333e9745520
admin <luciano101@msn.com>
2017-04-28 17:03:26 -0400

    progress so far carousel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -38,24 +38,38 @@ class Carousel extends React.Component {
 
                 <div className="carousel-inner" role="listbox">
                   <div className="item active">
+                    <div className="carousel-hero-area panel1">
+                      <div className="row">
+                        <div className="col-md-6 col-md-offset-4">
+                          <div className="text">
+                            <h2>FROM DC TO AC TO LEADING-EDGE</h2>
+                            <p>ADI's precision mixed-signal processing technology and system-level expertise are helping SolarEdge deliver a critical breakthrough in solar energy.
+                            </p>
+                            <strong>
+                              <a href="#">See how we're taking solar energy mainstream &#62;
+                              </a>
+                            </strong>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                     <img src={Banner1} alt="..." className="banner1"/>
                     <div className="carousel-caption">
                       
                     </div>
                   </div>
                   <div className="item">
-                    <div className="carousel-hero-area">
+                    <div className="carousel-hero-area panel2">
                       <div className="row">
                         <div className="col-md-6 col-md-offset-4">
                           <div className="text">
-                            <h2>THE CHIRP HEARD 'ROUND THE UNIVERSE</h2>
-                            <p>Analog Devices is proud to have its converters,
-                              temperature sensors, op amps and more utilized by LIGO
-                              - which listens to the universe of gravitational waves,
-                              ripples in the fabric of space-time.
+                            <h2>ULTRA LOW POWER ADUCM3029 MICROCONTROLLER</h2>
+                            <p>FOR IoT applications where power consumption, security,
+                              and robustness are key. Full-on at 30&#956;A/MHz, sleep at 
+                              750nA.
                             </p>
                             <strong>
-                              <a href="#">See the Innovations that changed astrophysics &#62;
+                              <a href="#">Better processing, control, and connectivity &#62;
                               </a>
                             </strong>
                           </div>
@@ -67,9 +81,9 @@ class Carousel extends React.Component {
                     </div>
                   </div>
                   <div className="item">
-                    <div className="carousel-hero-area">
+                    <div className="carousel-hero-area pannel3">
                       <div className="row">
-                        <div className="col-md-6 col-md-offset-4">
+                        <div className="col-md-12">
                           <div className="text">
                             <h2>THE CHIRP HEARD 'ROUND THE UNIVERSE</h2>
                             <p>Analog Devices is proud to have its converters,
